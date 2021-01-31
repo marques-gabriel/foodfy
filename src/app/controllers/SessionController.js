19d01bb816599ec9bdf82a9497b2641513d22dd3
Gabriel Marques <marquesgabriel@live.com>
2021-01-31 16:50:12 -0300

    Forgot and reset password form

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -2,6 +2,16 @@ module.exports = {
 
     loginForm(req, res) {
         return res.render("session/login")
+    },
+
+    forgotForm(req, res) {
+
+        return res.render("session/forgot-password")
+
+    },
+
+    resetForm(req, res) {
+        return res.render("session/password-reset", { token: req.query.token })
     }
 
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -7,6 +7,10 @@ const SessionController = require('../app/controllers/SessionController')
 // login/logout
 routes.get('/login', SessionController.loginForm)
 
+// reset password / forgot
+routes.get('/forgot-password', SessionController.forgotForm)
+routes.get('/password-reset', SessionController.resetForm)
+
 
 
 module.exports = routes
